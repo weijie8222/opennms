745c59867994b0ab9a8bb3df44d1da1d526dc0b2
Jesse White <jesse@opennms.org>
2017-09-19 17:31:40 -0400

    Better Minion handling.

--- features/collection/streaming/minion/src/main/java/org/opennms/netmgt/collection/streaming/minion/ListenerFactory.java
+++ features/collection/streaming/minion/src/main/java/org/opennms/netmgt/collection/streaming/minion/ListenerFactory.java
@@ -28,6 +28,8 @@
 
 package org.opennms.netmgt.collection.streaming.minion;
 
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Maps;
 import org.opennms.core.ipc.sink.api.AsyncDispatcher;
 import org.opennms.core.ipc.sink.api.MessageDispatcherFactory;
 import org.opennms.netmgt.collection.streaming.api.Listener;
@@ -36,8 +38,13 @@ import org.opennms.netmgt.collection.streaming.ipc.TelemetrySinkModule;
 import org.opennms.netmgt.collection.streaming.model.TelemetryMessage;
 import org.opennms.netmgt.collection.streaming.udp.UdpListener;
 import org.opennms.netmgt.dao.api.DistPollerDao;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedServiceFactory;
+import org.osgi.util.tracker.ServiceTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanWrapper;
@@ -46,12 +53,28 @@ import org.springframework.beans.PropertyAccessorFactory;
 import java.lang.reflect.Constructor;
 import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
+/**
+config:edit org.opennms.features.collection.streaming.listeners-udp-50001
+config:property-set name JTI
+config:property-set class-name org.opennms.netmgt.collection.streaming.udp.UdpListener
+config:property-set threads 5
+config:property-set queue.size 10000
+config:property-set batch.size 1000
+config:property-set batch.interval 500
+config:property-set listener.port 50001
+config:update
+ */
 public class ListenerFactory implements ManagedServiceFactory {
     private static final Logger LOG = LoggerFactory.getLogger(ListenerFactory.class);
 
+    private BundleContext bundleContext;
     private MessageDispatcherFactory messageDispatcherFactory;
     private DistPollerDao distPollerDao;
 
@@ -63,43 +86,36 @@ public class ListenerFactory implements ManagedServiceFactory {
     }
 
     @Override
-    public void updated(String pid, Dictionary properties) throws ConfigurationException {
-        LOG.debug("update({}, {})", pid, properties);
-
-        final TelemetryProtocol protocol = new TelemetryProtocol() {
-            @Override
-            public String getName() {
-                return "JTI";
+    public synchronized void updated(String pid, Dictionary properties) throws ConfigurationException {
+        final Listener existingListener = listenersByPid.get(pid);
+        if (existingListener != null) {
+            LOG.info("Updating existing listener with pid: {}", pid);
+            try {
+                existingListener.stop();
+            } catch (InterruptedException e) {
+                LOG.warn("Interruped while stopping listener with pid: {}", pid);
             }
+        } else {
+            LOG.info("Creating new listener for pid: {}", pid);
+        }
 
-            @Override
-            public Integer getNumThreads() {
-                return 1;
-            }
+        // Convert the dictionary to a map
+        final Iterator<String> keysIter = Iterators.forEnumeration(properties.keys());
+        final Map<String, String> parameters = Maps.toMap(keysIter, key -> (String)properties.get(key));
 
-            @Override
-            public Integer getBatchSize() {
-                return 1;
-            }
+        // Build the protocol definition
+        final MapBasedTelemetryProtocolDef protocol = new MapBasedTelemetryProtocolDef(parameters);
 
-            @Override
-            public Integer getBatchIntervalMs() {
-                return 0;
-            }
+        // Extract the keys from the map that are prefixed with "listener."
+        final Map<String, String> listenersParameters = filterKeysByPrefix(parameters, "listener.");
 
-            @Override
-            public Integer getQueueSize() {
-                return 1000;
-            }
-        };
         final TelemetrySinkModule sinkModule = new TelemetrySinkModule(protocol);
         sinkModule.setDistPollerDao(distPollerDao);
+        // FIXME: We need to make sure the dispatcher gets closed too
         final AsyncDispatcher<TelemetryMessage> dispatcher = messageDispatcherFactory.createAsyncDispatcher(sinkModule);
 
-        Map<String, String> props = new HashMap<>();
-        props.put("port", Integer.toString(50002));
         try {
-            final Listener listener = buildListener("JTI-UDP-50002", UdpListener.class.getCanonicalName(), props, dispatcher);
+            final Listener listener = buildListener(protocol.getName(), protocol.getClassName(), listenersParameters, dispatcher);
             listener.start();
             listenersByPid.put(pid, listener);
         } catch (Exception e) {
@@ -108,9 +124,8 @@ public class ListenerFactory implements ManagedServiceFactory {
     }
 
     @Override
-    public void deleted(String pid) {
-        LOG.debug("deleted({})", pid);
-        final Listener listener = listenersByPid.get(pid);
+    public synchronized void deleted(String pid) {
+        final Listener listener = listenersByPid.remove(pid);
         if (listener != null) {
             LOG.info("Stopping listener with pid: {}", pid);
             try {
@@ -121,18 +136,8 @@ public class ListenerFactory implements ManagedServiceFactory {
         }
     }
 
-    public void init() {
-        // Hack
-        try {
-            updated("init", null);
-        } catch (ConfigurationException e) {
-            LOG.error("Oops.", e);
-        }
-    }
-
-    public void stop() {
-        // TODO: Stop all
-        LOG.debug("stop()");
+    public synchronized void destroy() {
+        listenersByPid.keySet().forEach(pid -> deleted(pid));
     }
 
     // TODO: FIXME: Near duplicate of code in Telemetryd
@@ -173,4 +178,84 @@ public class ListenerFactory implements ManagedServiceFactory {
     public void setDistPollerDao(DistPollerDao distPollerDao) {
         this.distPollerDao = distPollerDao;
     }
+
+    public BundleContext getBundleContext() {
+        return bundleContext;
+    }
+
+    public void setBundleContext(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+    protected static Map<String, String> filterKeysByPrefix(Map<String, String> map, String prefix) {
+        // Extract the keys from the map that are prefixed
+        return map.keySet().stream()
+                .filter(k -> k.startsWith(prefix))
+                .collect(Collectors.toMap(k -> k.substring(prefix.length(), k.length()),
+                        k -> map.get(k)));
+    }
+
+    protected static class MapBasedTelemetryProtocolDef implements TelemetryProtocol {
+        private final String name;
+        private final String className;
+        private final Integer threads;
+        private final Integer queueSize;
+        private final Integer batchSize;
+        private final Integer batchInterval;
+
+        public MapBasedTelemetryProtocolDef(Map<String, String> parameters) {
+            name = getString("name", parameters);
+            className = getString("class-name", parameters);
+            threads = getOptionalInteger("threads", parameters);
+            queueSize = getOptionalInteger("queue.size", parameters);
+            batchSize = getOptionalInteger("batch.size", parameters);
+            batchInterval = getOptionalInteger("batch.interval", parameters);
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        public String getClassName() {
+            return className;
+        }
+
+        @Override
+        public Integer getNumThreads() {
+            return threads;
+        }
+
+        @Override
+        public Integer getBatchSize() {
+            return batchSize;
+        }
+
+        @Override
+        public Integer getBatchIntervalMs() {
+            return batchInterval;
+        }
+
+        @Override
+        public Integer getQueueSize() {
+            return queueSize;
+        }
+
+        private String getString(String key, Map<String, String> parameters) {
+            final String value = parameters.get(key);
+            if (value == null) {
+                throw new IllegalArgumentException(String.format("%s must be set.", key));
+            }
+            return value;
+        }
+
+        private Integer getOptionalInteger(String key, Map<String, String> parameters) {
+            final String strValue = parameters.get(key);
+            if (strValue == null) {
+                return null;
+            }
+            return Integer.parseInt(strValue);
+        }
+    }
+
 }
